07e106c4fd9aad50598442d1b4d146460d1eb11e
LaGabor <lagaborka@gmail.com>
2022-06-09 15:45:16 +0200

    Fixing issue in ProductController  in the if statement req changed to requestPage and the if statement and its else statement inside replaced with each other. Now NFT category have an index. indexPage.html get the header.html as a header.

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -28,6 +28,7 @@ public class ProductController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String requestPage = req.getParameter("type");
+        String headerTitle = req.getParameter("head");
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
@@ -36,24 +37,24 @@ public class ProductController extends HttpServlet {
         CartService cartService = new CartService(cartDataStore, productCategoryDataStore);
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-
+        context.setVariable("itemCount", cartService.getItemCount());
         if(requestPage==null){
             context.setVariable("searchType","Original");
             context.setVariable("category", productService.getProductCategory(1));
             context.setVariable("products", productService.getProductsForCategory(1));
-            context.setVariable("itemCount", cartService.getItemCount());
             engine.process("product/index.html", context, resp.getWriter());
             System.out.println(CartDaoMem.getInstance().getAll());
         }else{
+            context.setVariable("head",headerTitle);
             int index = Integer.parseInt(req.getParameter("id"));
             context.setVariable("searchType","Indexing");
             context.setVariable("category", requestPage);
             context.setVariable("itemCount", cartService.getItemCount());
-            if(req.equals("sup")){
-                context.setVariable("products", productService.getProductsForCategory(index));
+            if(requestPage.equals("sup")){
+                context.setVariable("products", productService.getProductsForSupplier(index));
                 engine.process("product/index.html", context, resp.getWriter());
             }else{
-                context.setVariable("products", productService.getProductsForSupplier(index));
+                context.setVariable("products", productService.getProductsForCategory(index));
                 engine.process("product/index.html", context, resp.getWriter());
             }
         }
--- src/main/webapp/static/js/indexPage.js
+++ src/main/webapp/static/js/indexPage.js
@@ -5,7 +5,7 @@ for (let i = 0; i < indexType.length; i++) {
     indexType[i].addEventListener('click',function () {
         console.log(indexType[i].dataset.id)
         console.log(indexType[i].dataset.index)
-        window.location.replace(`http://localhost:8080/?type=${indexType[i].dataset.index}&id=${indexType[i].dataset.id}`);
+        window.location.replace(`http://localhost:8080/?type=${indexType[i].dataset.index}&id=${indexType[i].dataset.id}&head=${indexType[i].innerHTML}`);
     });}
 
 
